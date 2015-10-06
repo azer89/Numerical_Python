20b2fd9ed9fd57bed0d5f7f14b5b77ad1222b9da
azer89 <radhitya@uwaterloo.ca>
2015-10-06 14:25:51 -0400

    added fixed-point method

--- rootfinding_methods.py
+++ rootfinding_methods.py
@@ -1,20 +1,39 @@
 # rootfinding_methods
 # rewritten from: https://gist.github.com/swvist/3775568
 
+import sys
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-# function f(x) x^3 - x - 2
+# function f(x)
 def my_func(x):    
     return x**3 - x - 2.0
+    
+# phi(x) = 0
+def phi(x):
+    return x**3 -  2.0
 
-# 1st derivative df(x)/dx = 3x^2 - 1
+# 1st derivative
 def dfdx(x):
     return 3.0 * x**2 - 1
     
+# fixed-point method
+def fixed_point(g, x0, tolerance = 0.000001, n_max = 1000) :
+    err = sys.float_info.max
+    for i in range(0, n_max):
+        x = g(x0)
+        print x
+        err = np.abs(x - x0)
+        
+        if err < tolerance :
+            return x        
+        
+        x0 = x
+        
+    
 # bisection method
-def bisection(f, a, b, tolerance = 0.000001, n_max = 100):
+def bisection(f, a, b, tolerance = 0.000001, n_max = 1000):
     for i in range(0, n_max) :    
         c = (a + b) / 2.0
         fc = f(c)
@@ -27,5 +46,8 @@ def bisection(f, a, b, tolerance = 0.000001, n_max = 100):
             
 if __name__ == "__main__":   
     root = bisection(my_func,1,2)
-    print 'the root is ', root
+    print '(bisection) the root is ', root
+    
+    root = fixed_point(phi, 0.5)
+    print '(fixed-point) the root is ', root
             
