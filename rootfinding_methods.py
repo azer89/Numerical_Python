9bfcdaeb42861758139f97d7345af29234e4bb8c
azer89 <radhitya@uwaterloo.ca>
2015-10-07 11:25:40 -0400

    added secant and netown's method

--- rootfinding_methods.py
+++ rootfinding_methods.py
@@ -1,5 +1,5 @@
 # rootfinding methods
-# inspired from: https://gist.github.com/swvist/3775568
+
 
 import sys
 import numpy as np
@@ -8,29 +8,65 @@ import matplotlib.pyplot as plt
 
 plt.ion()
 
+# default parameters
+default_tol = 0.000001
+default_n_max = 1000
+
 # function f(x)
 def my_func(x):
     return x - math.cos (x)
     #return x**3 - x - 2.0
     
-# phi(x) = x
+# phi(x) = x (for bisection method)
 def phi(x):
     return math.cos(x)    
     #return x**3 -  2.0
-
+    
+#def phi_newton(x):
+    #x - (math.cos(x) / -math.sin(x))
+    #return x + (1.0 / math.tan(x))
+#    return x - (np.sin(2.0*x))/(-1.0+np.cos(2.0* x))
+    
 # 1st derivative
 def dfdx(x):
     return math.sin(x) + 1.0    
     #return 3.0 * x**2 - 1
     
+def secant(f, x0, x1, tolerance = default_tol, n_max = default_n_max) :
+    err = sys.float_info.max
+    x_history = [x0, x1]
+    for i in range(0, n_max):     
+        x_1 = x_history[-1]
+        x_2 = x_history[-2]
+        x = x_1 - f(x_1) * ((x_1 - x_2) / ( f(x_1) - f(x_2) )) 
+        x_history.append(x)   
+        err = np.abs(x - x0)
+        
+        if err < tolerance or i == n_max - 1:
+            return x, x_history
+    
+
+# newton's method
+def newton(f, deriv, x0, tolerance = default_tol, n_max = default_n_max) :
+    err = sys.float_info.max
+    x_history = [x0]
+    for i in range(0, n_max):                
+        x = x0 - ( f(x0) / deriv(x0) )
+        x_history.append(x)  
+        err = np.abs(x - x0)
+        
+        if err < tolerance or i == n_max - 1:
+            return x, x_history        
+        
+        x0 = x
+    
 # fixed-point method
-def fixed_point(p, x0, tolerance = 0.000001, n_max = 1000) :
+def fixed_point(p, x0, tolerance = default_tol, n_max = default_n_max) :
     err = sys.float_info.max
     x_history = [x0]
     for i in range(0, n_max):
         x = p(x0)
-        x_history.append(x)        
-        #print x
+        x_history.append(x)  
         err = np.abs(x - x0)
         
         if err < tolerance or i == n_max - 1:
@@ -40,33 +76,28 @@ def fixed_point(p, x0, tolerance = 0.000001, n_max = 1000) :
         
     
 # bisection method
-def bisection(f, a, b, tolerance = 0.000001, n_max = 1000):
-    
+def bisection(f, a, b, tolerance = default_tol, n_max = default_n_max):    
     for i in range(0, n_max) :    
         c = (a + b) / 2.0
-        fc = f(c)
-        
-        #print fc
+        fc = f(c)        
         if np.abs(fc) < tolerance or np.abs(b - a) < tolerance  or i == n_max - 1:
             return c
         elif f(a) * fc > 0 :
             a = c
         else :
             b = c
-
+            
 # visualization
-def plot_phi_func(p, a, b, x_history):    
+def plot_fixed_point_method(p, a, b, x_history):    
     x_all = np.arange(a, b, 0.01)
     y_all = np.zeros(len(x_all))
     for i in range(0, len(x_all)):
         y_all[i] = p(x_all[i])
-    plt.plot(x_all, y_all, color='blue')
-    
+    plt.plot(x_all, y_all, color='blue')    
 
     y_x_history = np.zeros(len(x_history))
     for i in range(0, len(x_history)) :
-        y_x_history[i] = p(x_history[i])
-    plt.plot(x_history, y_x_history, 'ro')
+        y_x_history[i] = p(x_history[i])    
     
     # draw lines
     for i in range(0, len(y_x_history) - 1) :
@@ -81,20 +112,28 @@ def plot_phi_func(p, a, b, x_history):
             
         # draw a line from phi(x_n) to x_n = y_n
         plt.plot([cur_x, cur_y], [cur_y, cur_y], 'k-', lw=0.5)
+        
         # draw a line from x_n = y_n to phi(x_{n+1})
         plt.plot([cur_y, next_x], [cur_y, next_y], 'k-', lw=0.5)
         
     plt.plot([0, 1], [0, 1], 'k-', lw=0.5)
+    plt.plot(x_history, y_x_history, 'ro')
     
     plt.show()
     
             
 if __name__ == "__main__":   
-    root = bisection(my_func, 0.0, 1.0)
-    print '(bisection) the root is ', root
     
-    root, x_history = fixed_point(phi, 0.5)
-    print '(fixed-point) the root is ', root
+    #root = bisection(my_func, 0.0, 1.0)
+    #print '(bisection) the root is ', root
     
-    plot_phi_func(phi, 0.0, 1.0, x_history)
+    #root, x_history = fixed_point(phi, 0.5)
+    #print '(fixed-point) the root is ', root    
+    #plot_fixed_point_method(phi, 0.0, 1.0, x_history)
+    
+    root, x_history = newton(my_func, dfdx, 0.5)
+    print '(newton) the root is ', root    
+    
+    #root, x_history = secant(my_func, 0.4, 0.6)
+    #print '(secant) the root is ', root   
             
