27179baa53d4306fda51a4b74acb4c7bbd0aee07
azer89 <radhitya@uwaterloo.ca>
2015-10-14 09:32:29 -0400

    added comments

--- eigenvalue_polynomials.py
+++ eigenvalue_polynomials.py
@@ -1,38 +1,39 @@
 
 '''
+Find all roots of a polynomial:
+a_{n} x^{n} + a_{n-1} x^{n-1} + a_{n-2} x^{n_2} + ... + a_{1} x + a_{0}
+
 Reza Adhitya Saputra
+Computer Science Department - University of Waterloo
 radhitya@uwaterloo.ca
 '''
 
 import numpy as np
 from numpy import linalg as la
 
-'''
-Find all roots of a polynomial:
-
-a_{n} x^{n} + a_{n-1} x^{n-1} + a_{n-2} x^{n_2} + ... + a_{1} x + a_{0}
-
-'''
-
-# An array of a_{n}, a_{n-1}, ..., a_{0}
+# The input of an array of a_{n}, a_{n-1}, ..., a_{0}
 a_array = np.array([5.0, 4.0, 3.0, 2.0, 1.0])
 
 # the value of a_{n}
 an = a_array[0]
 
-# normalized a_array, this is the top row of C matrix
+# normalized a_array
 c_array = np.reshape(a_array[1:len(a_array)] / an, (1, len(a_array) - 1))
 
-# make all elements to be negative
+# make all elements to be negative, this is the top row of C matrix
 c_array = -c_array
 
-# here we want to create C matrix, where C . x = lambda . x
+# here we create the C matrix, where C . x = lambda . x
 r_zeros = np.zeros(len(a_array) - 2)
-r_zeros = np.reshape(r_zeros, (len(r_zeros), 1))
-id_mat = np.matrix(np.identity(len(a_array) - 2))
-id_mat_extended = np.concatenate((id_mat, r_zeros), axis=1)
+r_zeros = np.reshape(r_zeros, (len(r_zeros), 1))  # the rightmost column
+id_mat = np.matrix(np.identity(len(a_array) - 2)) # the identity matrix
+id_mat_extended = np.concatenate((id_mat, r_zeros), axis=1) # the C matrix minus the first row
 
+# C matrix
 c_mat = np.concatenate((c_array, id_mat_extended), axis=0)
 
 # obtain the eigen values and the eigen vector
 eigen_vals, eigen_vector = la.eig(c_mat)
+
+print "the solution is: "
+print eigen_vals
--- vector_newton.py
+++ vector_newton.py
@@ -1,20 +1,33 @@
-# vector newton
+
+
+'''
+Vector Newton - Finding the solution of a linear system of equations
+
+Reza Adhitya Saputra
+Computer Science Department - University of Waterloo
+radhitya@uwaterloo.ca
+'''
 
 import numpy as np
 
+'''
+vector-valued f(x), where x is a vector
+'''
 def my_f(x):
-    fx = np.zeros(len(x))    
-    
     x_1 = x[0]
     x_2 = x[1]
     x_3 = x[2]
     
+    fx = np.zeros(len(x))
     fx[0] = x_1**2.0 + x_2**2.0 - x_3 - 1.0
     fx[1] = x_1 + x_2 * x_3
     fx[2] = np.exp(x_1) - 0.5
     
     return fx
-    
+
+'''
+The jacobian matrix of f(x), x should be a vector
+'''
 def my_Jacobian(x):
     x_1 = x[0]
     x_2 = x[1]
@@ -39,31 +52,49 @@ def my_Jacobian(x):
     
     return j_matrix
     
-def VectorNewtonsMethod(fcn, Jacobian, x_orig, xtol = 10e-7):
-    x_history = [x_orig]    
-    x_0 = x_orig
-    for i in range(0, 20):    
-        # solve J . delta_x = -fx
-        j_matrix = Jacobian(x_0)
-        min_fx = -fcn(x_0)
-        delta_x = np.linalg.solve(j_matrix, min_fx)
-        
-        x_next = x_0 + delta_x
-        x_history.append(x_next)
+'''
+vector newton
 
-        d = np.linalg.norm(x_next - x_0)
-        #print d        
+note that the last parameter is tolerance
+'''
+def VectorNewtonsMethod(fcn, Jacobian, x_orig, xtol = 10e-7):
+    x_history = [x_orig]   # a list containing the appoximate solutions
+    x_0 = x_orig
+    
+    # the loop is only 20 times
+    for i in range(0, 20):           
+        j_matrix = Jacobian(x_0)    # ontain the jacobian matrix
+        min_fx = -fcn(x_0)          # obtain -f(x)
+        delta_x = np.linalg.solve(j_matrix, min_fx) # solve J . delta_x = -fx
+        
+        x_next = x_0 + delta_x      # obtain x_{n+1} = x_{n} + delta_x
+        x_history.append(x_next)    # store x values
+
+        d = np.linalg.norm(x_next - x_0)    # get the euclidean distance   
         if d < xtol :
+            # the distance is less than the tolerance
             print "SUCCESS"
             return x_history        
         
+        # update
         x_0 = x_next
-            
+    
+    # stop after 20 iterations
+    # it's sad, since we are not close enough to the solution
     print "FAILED"
     return x_history
 
-x_0 = np.array([0.4, 0.0, 0.9])    # FAILED
-#x_0 = np.array([0.0, 1.0, 0.0])    # SUCCESS
-#x_0 = np.array([-0.69, 1.07, 0.64]) # this is really close to the solution
+'''
+These below are bunch of initial values,
+uncomment one of them
+'''
+#x_0 = np.array([0.4, 0.0, 0.9])    # FAILED
+x_0 = np.array([0.0, 1.0, 0.0])    # SUCCESS
+#x_0 = np.array([-0.69, 1.07, 0.64]) # SUCCESS, this is really close to the solution
+
+# calculate the solution
 x_history = VectorNewtonsMethod(my_f, my_Jacobian, x_0)
-print "the solution is :", x_history[-1]
+xstar = x_history[-1] # the solution is the last element of the list
+
+print "the solution is :", xstar
+
