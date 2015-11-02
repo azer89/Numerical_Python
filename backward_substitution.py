9298fbed7adf3a991880c14369ca63ba714fc85f
azer89 <radhitya@uwaterloo.ca>
2015-11-02 11:04:05 -0500

    added forward  substitution

--- backward_substitution.py
+++ backward_substitution.py
@@ -3,3 +3,18 @@
 Backward substitution
 '''
 
+import numpy as np
+
+L = np.array([[1.0, 0.0], 
+              [1.0, 1.0]])
+              
+row, col = np.shape(L)
+              
+b = np.array([1.0, 10.0])
+
+y = b.copy()
+
+for i in range(0, col):   # iterate rows
+    for j in range(0, i): # iterate columns
+        y[i] = y[i] - L[i][j] * y[j]
+        
