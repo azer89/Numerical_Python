6864d740809a437027b79c46241a9ea360f8feaa
azer89 <radhitya@uwaterloo.ca>
2015-11-02 12:37:45 -0500

    edited forward nd backward substitutions

--- backward_substitution.py
+++ backward_substitution.py
@@ -5,21 +5,31 @@ Backward substitution
 
 import numpy as np
 
-L = np.array([[3.0, 1.0, 1.0], 
+# In[]
+def backsub(U, b):
+    y = b.copy()
+
+    # last row
+    y[col - 1] = y[col - 1] / U[col - 1][col - 1]
+    
+    for i in range(col - 2, -1, -1):
+        for j in range(col - 1, i, -1):
+            y[i] = y[i] - U[i][j] * y[j]
+        y[i] = y[i] / U[i][i]
+    
+    return y
+    
+# In[]
+    
+U = np.array([[3.0, 1.0, 1.0], 
               [0.0, 2.0, 1.0],
               [0.0, 0.0, 2.0]])
               
-row, col = np.shape(L)
+row, col = np.shape(U)
 
 b = np.array([3.0, 2.0, 1.0])
 
-y = b.copy()
+y = backsub(U, b)
 
-# last row
-y[col - 1] = y[col - 1] / L[col - 1][col - 1]
 
-for i in range(col - 2, -1, -1):
-    for j in range(col - 1, i, -1):
-        print y[i], "- ", L[i][j], " * ", y[j]
-        y[i] = y[i] - L[i][j] * y[j]
-    y[i] = y[i] / L[i][i]
+
--- forward_substitution.py
+++ forward_substitution.py
@@ -5,6 +5,23 @@ Forward substitution
 
 import numpy as np
 
+# In[]
+
+def forwardsub(L, b):
+    y = b.copy()
+
+    # first row
+    y[0] = y[0] / L[0][0]
+    
+    for i in range(1, col):   # iterate rows
+        for j in range(0, i): # iterate columns
+            y[i] = y[i] - L[i][j] * y[j]
+        y[i] = y[i] / L[i][i]
+    
+    return y
+    
+# In[]
+
 L = np.array([[2.0, 0.0], 
               [1.0, 2.0]])
               
@@ -12,13 +29,7 @@ row, col = np.shape(L)
               
 b = np.array([1.0, 10.0])
 
-y = b.copy()
+y = forwardsub(L, b)
 
-# first row
-y[0] = y[0] / L[0][0]
 
-for i in range(1, col):   # iterate rows
-    for j in range(0, i): # iterate columns
-        y[i] = y[i] - L[i][j] * y[j]
-    y[i] = y[i] / L[i][i]
     
