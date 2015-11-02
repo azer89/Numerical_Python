3f201c8323f5fa77676250a5b1e282a02da4c548
azer89 <radhitya@uwaterloo.ca>
2015-11-02 12:02:06 -0500

    forward nd backward substitutions

--- backward_substitution.py
+++ backward_substitution.py
@@ -5,16 +5,21 @@ Backward substitution
 
 import numpy as np
 
-L = np.array([[1.0, 0.0], 
-              [1.0, 1.0]])
+L = np.array([[3.0, 1.0, 1.0], 
+              [0.0, 2.0, 1.0],
+              [0.0, 0.0, 2.0]])
               
 row, col = np.shape(L)
-              
-b = np.array([1.0, 10.0])
+
+b = np.array([3.0, 2.0, 1.0])
 
 y = b.copy()
 
-for i in range(0, col):   # iterate rows
-    for j in range(0, i): # iterate columns
+# last row
+y[col - 1] = y[col - 1] / L[col - 1][col - 1]
+
+for i in range(col - 2, -1, -1):
+    for j in range(col - 1, i, -1):
+        print y[i], "- ", L[i][j], " * ", y[j]
         y[i] = y[i] - L[i][j] * y[j]
-        
+    y[i] = y[i] / L[i][i]
--- forward_substitution.py
+++ forward_substitution.py
@@ -3,4 +3,22 @@
 Forward substitution
 '''
 
-L = 
+import numpy as np
+
+L = np.array([[2.0, 0.0], 
+              [1.0, 2.0]])
+              
+row, col = np.shape(L)
+              
+b = np.array([1.0, 10.0])
+
+y = b.copy()
+
+# first row
+y[0] = y[0] / L[0][0]
+
+for i in range(1, col):   # iterate rows
+    for j in range(0, i): # iterate columns
+        y[i] = y[i] - L[i][j] * y[j]
+    y[i] = y[i] / L[i][i]
+    
