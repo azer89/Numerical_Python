e9bf2cf4ecb4a15b6863248100502aa8a878fef6
azer89 <radhitya@uwaterloo.ca>
2015-11-15 13:00:10 -0500

    inverse dft

---
+++ inv_dft.py
@@ -0,0 +1,25 @@
+'''
+Inverse Discrete Fourier Transform
+'''
+
+import numpy as np
+import matplotlib.pylab as plt
+
+N = 200
+F = np.zeros(N, dtype=complex)
+
+F[5] = complex(1)
+#F[N-1] = complex(1, 1)
+
+f = np.zeros(N, dtype=complex)
+for k in range(N): 
+    for n in range(N):
+        f[k] += F[n] * np.exp(2.0j * np.pi * n * k / float(N))
+
+f = f / float(N)
+f_abs = np.absolute(f)
+
+x = np.arange(0, 1, 1.0 / float(N))
+plt.clf()
+plt.plot(x, np.real(f), 'r') 
+plt.show()
