fe26f6336d209361aabb49d42b934c2cde46c93f
azer89 <radhitya@uwaterloo.ca>
2015-11-30 08:35:21 -0500

    edited

--- minimization_example.py
+++ minimization_example.py
@@ -9,5 +9,6 @@ import scipy.optimize as optimize
 def f(c):
     return np.sqrt(c[0]**2 + c[1]**2 + c[2]**2)
 
-result = optimize.minimize(f, [1,1,1])
-print(result)
+result = optimize.minimize(f, [0.0, 0.0, 0.0])
+sol = result.x
+print(sol)
