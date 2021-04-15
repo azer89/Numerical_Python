9e7fae7dff51917046bdcc4c672a5dd99cbb2505
Reza Saputra <radhitya@uwaterloo.ca>
2021-04-15 07:59:48 -0400

    Update euler_method.py

--- euler_method.py
+++ euler_method.py
@@ -2,43 +2,40 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-from wiki
-
-y'(t) = f(t,y(t))
-
-y(t_0)=y_0
-
-y_{n+1} = y_n + hf(t_n,y_n)
-'''
 
 # right hand side of the ODE
 def f(x, y) :
-    return y + 1 # x doesn't matter since dy/dx is linear
-
+    # because the derivative of e^x is e^2
+    return y;
+    
 # initial condition
-y0 = 0
+y0 = 1
 
 # time step
-h = 0.01
+h = 0.1
 
 # solve from 0 to time T
-T = 10
+T = 5
 
 # list of discretized time (visualization purpose only) 
 x = np.linspace(0, T, int(T/h) + 1)
 
-# solution
-y = np.zeros(len(x)) 
+# Oiler solution
+y = np.zeros(len(x))
 
+# initial condition
 y[0] = y0;
 
-for i in xrange(1, len(x)) :
-    y[i] = y[i - 1] + f(x[i - 1], y[i - 1]) * h
+# let's compute oiler method
+for i in range(1, len(x)):
+    y[i] = y[i-1] + f(x[i-1],y[i-1])*h
+  
+# the true solution
+y_true = np.exp(x)
     
 plt.figure()
-
-plt.plot(x, y, color='red')
+plt.plot(x,y,'b.-',x,y_true,'r-')
+plt.legend(['Euler method','True e^x'])
 plt.show()
     
 
